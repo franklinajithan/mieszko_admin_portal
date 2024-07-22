14fa44dab2d9b3ee858b3a6e20457971733107ce
franklinajithan <franklin.ajithan@gmail.com>
2024-07-22 10:54:49 +0100

    Working fine typescript

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Unable to find root element with id 'root'");
+}
+
+function logWebVitals(metric: any) {
+  console.log(metric);
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(logWebVitals);
